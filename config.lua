920cb1fd8fa4e51eaa17755042633fbe3a3803cc
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-10-22 15:42:34 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,19 +1,18 @@
 Config = {}
 
--- Activer le debug pour afficher les zones en jeu
+-- Active ou désactive le debug (mur vert en jeu)
 Config.Debug = true
 
--- Liste des polyzones
+-- Définir toutes les polyzones ici
 Config.PolyZones = {
-    test = {
+    MechanicThunderShop = {
+        clearVehicles = true, -- supprimer véhicules NPC
+        clearPeds = true,     -- supprimer peds NPC
         points = {
-            {x = -2054.4846, y = -432.3635},
-            {x = -1989.6630, y = -485.7856},
-            {x = -2051.7283, y = -562.8635},
-            {x = -2120.6768, y = -510.3206}
-        },
-        clearVehicles = true,
-        clearPeds = true,
-        radiusCheck = 150.0 -- rayon pour optimiser la recherche
+            {x = -2054.4846, y = -432.3635, z = 11.5195},
+            {x = -1989.6630, y = -485.7856, z = 11.6392},
+            {x = -2051.7283, y = -562.8635, z = 5.6088},
+            {x = -2120.6768, y = -510.3206, z = 2.3494}
+        }
     }
 }
