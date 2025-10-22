9887c6663fc01997e5a434a108ab12c64e665146
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-10-22 14:53:06 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TRAPZY'
+description 'Polyzone NPC Script - Cleaning vehicles and peds with debug'
+
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
